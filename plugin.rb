55f81032e0fb90724c1b9bd5f8b27c1634027c37
root <root@Legion5Pro.localdomain>
2025-08-23 20:08:51 +0200

    Update author name in plugin metadata

--- plugin.rb
+++ plugin.rb
@@ -3,8 +3,8 @@
 # name: discourse-toc
 # about: Table of Contents plugin for Discourse posts
 # version: 0.1.0
-# authors: Your Name
-# url: https://github.com/yourusername/discourse-toc
+# authors: Bartlomiej Wolk
+# url: https://github.com/bartlomiejwolk/discourse-toc
 
 after_initialize do
   # Plugin initialization code will go here
